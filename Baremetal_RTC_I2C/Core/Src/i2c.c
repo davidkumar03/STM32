8bb74b23efca2cc144f63442a230a44f3102a5cf
David Kumar <davidkumar03@gmail.com>
2026-01-13 17:45:44 +0530

    DS3231 RTC over I2C

---
+++ Baremetal_RTC_I2C/Core/Src/i2c.c
@@ -0,0 +1,207 @@
+/*
+ * i2c.c
+ *
+ *  Created on: Jun 4, 2025
+ *      Author: davidkumar
+ */
+
+
+#include "stm32f405xx.h"
+#include <stdint.h>
+#include "i2c.h"
+
+
+#define GPIOBEN (1U<<1)
+#define I2C2EN  (1U<<22)
+#define I2C_100_KHZ  80
+#define SM_MODE_MAX_RISE_TIME   17
+#define SR_BUSY   (1U<<1)
+#define START_CN  (1U<<8)
+#define START_FLAG (1U<<0)
+#define ADDR_FLAG  (1U<<1)
+#define BTF_FLAG   (1U<<2)
+#define RXNE_FLAG  (1U<<6)
+#define SR1_TXE    (1U<<7)
+#define ACK_FLAG   (1U<<10)
+#define STOP_CN     (1U<<9)
+
+
+void init_i2c(void)
+{
+	//Enable Clock access GPIOB
+	RCC->AHB1ENR |=GPIOBEN;
+	//Set PB10 Alternate function
+	GPIOB->MODER |=(1U<<21);
+	GPIOB->MODER &=~(1U<<20);
+	//set PB11 Alternate function
+	GPIOB->MODER |=(1U<<23);
+	GPIOB->MODER &=~(1U<<22);
+	//set PB10 & PB11 open drain
+	GPIOB->OTYPER |=(1U<<10);
+	GPIOB->OTYPER |=(1U<<11);
+	//set PB10 Connected as Pull up resistor
+	GPIOB->PUPDR &=~(1U<<21);
+	GPIOB->PUPDR |=(1U<<20);
+	//set PB11 connected as pull up resistor
+	GPIOB->PUPDR &=~(1U<<23);
+	GPIOB->PUPDR |=(1U<<22);
+	//set PB10 Alternate function AF4(I2C)
+	GPIOB->AFR[1] &=~(1U<<11);
+	GPIOB->AFR[1] |=(1U<<10);
+	GPIOB->AFR[1] &=~(1U<<9);
+	GPIOB->AFR[1] &=~(1U<<8);
+	//set PB11 Alternate function AF4(I2C)
+	GPIOB->AFR[1] &=~(1U<<15);
+	GPIOB->AFR[1] |=(1U<<14);
+	GPIOB->AFR[1] &=~(1U<<13);
+	GPIOB->AFR[1] &=~(1U<<12);
+	//Clock Enable I2C2
+	RCC->APB1ENR |=I2C2EN;
+	//Enter Reset Mode
+	I2C2->CR1 |=(1U<<15);
+	//Release a Reset Mode
+	I2C2->CR1 &=~(1U<<15);
+	//set peripheral clock frequency
+	I2C2->CR2 = (1U<<4); //16MHZ
+	//set I2C Standard mode 100KHZ frequency
+	I2C2->CCR = I2C_100_KHZ;
+    //set tr Time
+	I2C2->TRISE = SM_MODE_MAX_RISE_TIME;
+	//Enable I2C2 Module
+	I2C2->CR1 = (1U<<0);
+}
+
+
+
+void  i2c_readByte(uint8_t saddr,uint8_t maddr,uint8_t *data)
+{
+	//Temporary variable
+	volatile int temp;
+	//Check the Bus busy or IDLE
+	while(I2C2->SR2 & (SR_BUSY)){}
+	//Generate Start condition
+	I2C2->CR1 |=START_CN;
+	//wait until start flag is set
+	while(!(I2C2->SR1 & (START_FLAG))){}
+	//Transmit slave address with write
+	I2C2->DR = saddr<<1 | 0;
+	//wait till address flag is set
+	while(!(I2C2->SR1 & (ADDR_FLAG))){}
+	//clear address flag
+	temp=I2C2->SR2;
+	//send memory address
+	I2C2->DR=maddr;
+	//wait until transmitter empty
+	while(!(I2C2->SR1 & (SR1_TXE))){}
+	//Generate Restart
+	I2C2->CR1 |=START_CN;
+	//wait until start flag is set
+	while(!(I2C2->SR1 & (START_FLAG))){}
+	//Transmit slave address with read
+	I2C2->DR = saddr<<1 | 1;
+	//wait till address flag is set
+	while(!(I2C2->SR1 & (ADDR_FLAG))){}
+	//Disable Acknowledge
+	I2C2->CR1 &=~ ACK_FLAG;
+	//clear address flag
+	temp=I2C2->SR2;
+	//Generate stop condition if data received
+	I2C2->CR1 |=STOP_CN;
+	//Wait to until set RXNE bit
+	while(!(I2C2->SR1 & (RXNE_FLAG))){}
+	//Read the Data from Buffer
+	*data++ = I2C2->DR;
+}
+void i2c_ReadMulti(uint8_t saddr,uint8_t maddr, uint8_t n, uint8_t* data)
+{
+	//Temporary variable
+	volatile int temp;
+	//Check the Bus busy or IDLE
+	while(I2C2->SR2 & (SR_BUSY)){}
+	//Generate Start condition
+	I2C2->CR1 |=START_CN;
+	//wait until start flag is set
+	while(!(I2C2->SR1 & (START_FLAG))){}
+	//Transmit slave address with write
+	I2C2->DR = saddr<<1 | 0;
+	//wait till address flag is set
+	while(!(I2C2->SR1 & (ADDR_FLAG))){}
+	//clear address flag
+	temp=I2C2->SR2;
+	//send memory address
+	I2C2->DR=maddr;
+	//wait until transmitter empty
+	while(!(I2C2->SR1 & (SR1_TXE))){}
+	//Generate Restart
+	I2C2->CR1 |=START_CN;
+	//wait until start flag is set
+	while(!(I2C2->SR1 & (START_FLAG))){}
+	//Transmit slave address with read
+	I2C2->DR = saddr<<1 | 1;
+	//wait till address flag is set
+	while(!(I2C2->SR1 & (ADDR_FLAG))){}
+	//clear address flag
+	temp=I2C2->SR2;
+	//Enable Acknowledge
+	I2C2->CR1 |= ACK_FLAG;
+
+	while(n>0u)
+	{
+		//If is it equal to One byte
+		if(n==1U)
+		{
+			//Disable Acknowledge
+			I2C2->CR1 &=~ ACK_FLAG;
+			//Generate stop condition if data received
+			I2C2->CR1 |=STOP_CN;
+			//Wait to until set RXNE bit
+			while(!(I2C2->SR1 & (RXNE_FLAG))){}
+			//Read the Data from Buffer
+			*data++ = I2C2->DR;
+			break;
+
+		}
+		else
+		{
+			//Wait to until set RXNE bit
+			while(!(I2C2->SR1 & (RXNE_FLAG))){}
+			//Read the Data from Buffer
+			(*data++) = I2C2->DR;
+
+			n--;
+		}
+	}
+}
+
+void i2c_WriteMulti(uint8_t saddr,uint8_t maddr,uint8_t *buffer, uint8_t length)
+{
+	//Temporary variable
+	volatile int temp;
+	//Check the Bus busy or IDLE
+	while(I2C2->SR2 & (SR_BUSY)){}
+	//Generate Start condition
+	I2C2->CR1 |=START_CN;
+	//wait until start flag is set
+	while(!(I2C2->SR1 & (START_FLAG))){}
+	//Transmit slave address with write
+	I2C2->DR = saddr<<1 | 0;
+	//wait till address flag is set
+	while(!(I2C2->SR1 & (ADDR_FLAG))){}
+	//clear address flag
+	temp=I2C2->SR2;
+	//wait until transmitter empty
+	while(!(I2C2->SR1 & (SR1_TXE))){}
+	//send memory address
+	I2C2->DR=maddr;
+	for(int i=0;i<length;i++)
+	{
+		//wait until transmitter empty
+		while(!(I2C2->SR1 & (SR1_TXE))){}
+		//Transmit Memory Address
+		I2C2->DR = *buffer++;
+	}
+	//wait until transfer finished
+	while(!(I2C2->SR1 & BTF_FLAG)){}
+	//Generate stop condition if data Transfered
+	I2C2->CR1 |=STOP_CN;
+}
---
+++ Baremetal_RTC_I2C/Core/Src/main.c
@@ -0,0 +1,118 @@
+#include "stm32f405xx.h"
+#include "i2c.h"
+
+#define GPIOAEN   (1U<<0)
+#define GPIOBEN   (1U<<1)
+#define clock     16000000U
+#define baud_rate 115200U
+
+uint8_t rtc[5];
+uint8_t newline[]="\r\n";
+uint8_t seconds,minutes,hours,day,date;
+uint8_t ch=58;
+uint8_t time[3] = {
+    0x00,  // seconds
+    0x00,  // minutes
+    0x11   // hours (11 BCD)
+};
+
+
+void HSI_Init(void)
+{
+    // 1. Enable GPIOA clock
+    RCC->AHB1ENR |= (1 << 0);
+
+    // 2. Set PA8 to Alternate Function mode
+    GPIOA->MODER &= ~(0x3 << (8 * 2));
+    GPIOA->MODER |=  (0x2 << (8 * 2));  // AF mode
+
+    // 3. Select AF0 (MCO1)
+    GPIOA->AFR[1] &= ~(0xF << ((8 - 8) * 4));
+    // No need to set because AF0 = 0
+    // MCO1 prescaler = /1 (full HSI)
+    RCC->CFGR &= ~(0x7 << 24);  // Clear MCO1PRE
+    RCC->CFGR |=  (0x0 << 24);  // divide by 1
+
+    // 4. Configure MCO1 in RCC->CFGR
+    RCC->CFGR &= ~(0x3 << 21);   // Clear bits 22:21
+    RCC->CFGR |=  (0x0 << 21);   // 00 = HSI clock
+}
+void uart_tx_init(void)
+{
+    RCC->AHB1ENR |= GPIOAEN;
+
+    // PA2 alternate function AF7
+    GPIOA->MODER &= ~(3U << (2*2));
+    GPIOA->MODER |=  (2U << (2*2));
+    GPIOA->AFR[0] &= ~(0xF << (4*2));
+    GPIOA->AFR[0] |=  (7   << (4*2));
+
+    RCC->APB1ENR |= (1U<<17);
+
+    USART2->BRR = (clock + (baud_rate/2U))/baud_rate;
+    USART2->CR1 |= (1U<<3);   // TE
+    USART2->CR1 |= (1U<<13);  // UE
+}
+
+void uart_write(uint8_t *ch)
+{
+    int i=0;
+    while(ch[i] != '\0')
+    {
+        while(!(USART2->SR & (1U<<7))){};
+        USART2->DR = (ch[i] & 0xFF);
+        i++;
+    }
+}
+
+void uart_write_char(uint8_t c)
+{
+    while(!(USART2->SR & (1U<<7))){};
+    USART2->DR = (c & 0xFF);
+}
+
+void uart_write_int(uint32_t num)
+{
+    char buf[12];
+    int i=0, j;
+    if(num == 0){ uart_write("0"); return; }
+    while(num > 0)
+    {
+        buf[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+    for(j=i-1; j>=0; j--)
+    {
+        while(!(USART2->SR & (1U<<7))){};
+        USART2->DR = buf[j];
+    }
+}
+
+void uart_display(void)
+{
+	seconds =((rtc[0] >> 4) & 0x07) * 10 + (rtc[0] & 0x0F);
+	minutes =((rtc[1] >> 4) & 0x07) * 10 + (rtc[1] & 0x0F);
+	hours =((rtc[2] >> 4) & 0x07) * 10 + (rtc[2] & 0x0F);
+	uart_write_int(hours);
+	uart_write_char(ch);
+	uart_write_int(minutes);
+	uart_write_char(ch);
+	uart_write_int(seconds);
+	uart_write(newline);
+}
+
+int main(void)
+{
+	HSI_Init();
+	uart_tx_init();
+	init_i2c();
+   // i2c_readByte(0x68,0x01,rtc);
+	//We Assigned time 11 AM
+	i2c_WriteMulti(0x68,0x00,time,3);
+    while(1)
+    {
+    	i2c_ReadMulti(0x68,0x00,3,rtc);
+    	uart_display();
+    }
+	return 0;
+}
